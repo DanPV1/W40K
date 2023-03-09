1b296c7c1e982ee6a410f5e3b44c6f0745988b96
DanPV1 <118803662+DanPV1@users.noreply.github.com>
2023-03-09 12:47:07 +0100

    Add files via upload

---
+++ js/function.js
@@ -0,0 +1,39 @@
+/*********************************************************************/
+/*  Web del Romanticismo  - FUNCIONES DE JS PARA SNIPPERTS           */
+/*  CSS de Autores románticos                                        */
+/*  Autor: Daniel de Poorter Vázquez                                 */
+/*  versión 1.0                                                      */
+/*********************************************************************/
+
+let slideIndex = [1,2];
+let slideId = ["mySlides1", "mySlides2", "mySlides3"]
+showSlides(1, 0);
+showSlides(1, 1);
+showSlides(1, 2);
+
+function plusSlides(n, no) {
+  showSlides(slideIndex[no] += n, no);
+}
+
+function showSlides(n, no) {
+  let i;
+  let x = document.getElementsByClassName(slideId[no]);
+  if (n > x.length) {slideIndex[no] = 1}
+  if (n < 2) {slideIndex[no] = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex[no]-1].style.display = "block";
+}
+
+/* ***************************************************/
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+} 
